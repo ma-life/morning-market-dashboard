80fd083f953447c62c3476e72560efc1502a9c21
ma-life <smc.malife@gmail.com>
2026-06-11 09:51:21 +0900

    Fix: Adjust top padding to prevent title clipping

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 st.markdown("""
     <style>
         .block-container {
-            padding-top: 0rem;
+            padding-top: 3.5rem;
             padding-bottom: 0rem;
             padding-left: 0rem;
             padding-right: 0rem;
